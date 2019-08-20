e74015302c79a15842b081b1a81b57c7759a657d
Rose <50706764+Edestiny7@users.noreply.github.com>
2019-08-19 22:51:27 -0400

    New API key change

--- assets/javascript/ingredients.js
+++ assets/javascript/ingredients.js
@@ -11,11 +11,7 @@ $(document).ready(function() {
 
     $("#img-div").append(dishImage)
 
-<<<<<<< HEAD
     let recipeKey = "c9ae7f2ea0ea9d5aab1fec2f528198a8";
-=======
-    let recipeKey = "69cfc8b380c2e920a7f5e117b2b5bd19";
->>>>>>> ce2c261e0d42a3bd0d5e3797e4e02ff5d2eedf95
 
     //Get recipe Id from local storage
     var recipeID = localStorage.getItem("ingredients");
@@ -27,30 +23,18 @@ $(document).ready(function() {
         var queryURL = "https://www.food2fork.com/api/get?rId=" + recipeID + "&key=" + recipeKey;
 
         $.ajax({
-<<<<<<< HEAD
                 url: queryURL,
                 method: "GET"
             })
             .then(function(response) {
-=======
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
->>>>>>> ce2c261e0d42a3bd0d5e3797e4e02ff5d2eedf95
 
                 console.log(response);
                 let parsedResponseObject = response; //change response to ingredients1 to response only here to switch to dummy object
                 parsedResponseObject = JSON.parse(response);
-<<<<<<< HEAD
 
                 // parsedResponseObject = JSON.parse(parsedResponseObject);
                 console.log(parsedResponseObject);
 
-=======
-                // parsedResponseObject = JSON.parse(parsedResponseObject);
-                console.log(parsedResponseObject);
->>>>>>> ce2c261e0d42a3bd0d5e3797e4e02ff5d2eedf95
                 let ingredientsArray = parsedResponseObject.recipe.ingredients;
                 console.log(ingredientsArray)
 
@@ -64,16 +48,7 @@ $(document).ready(function() {
                 let recipeName = $("<div>").text(parsedResponseObject.recipe.title).css("margin-top", "10px").addClass("d - flex justify - content - center");​
                 $("#recipeName").append(recipeName);
 
-<<<<<<< HEAD
                 $(document).on("click", ".instructions-link", function() {
-=======
-                let recipeName = $("<div>").text(parsedResponseObject.recipe.title).css("margin-top", "10px").addClass("d - flex justify - content - center");
-
-                $("#recipeName").append(recipeName);
-
-
-                $(document).on("click", ".instructions-link", function () {
->>>>>>> ce2c261e0d42a3bd0d5e3797e4e02ff5d2eedf95
                     window.open(parsedResponseObject.recipe.source_url);
                 })
 
